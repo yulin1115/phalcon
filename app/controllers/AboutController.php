1c7e393cd27f2a1774a8c927c44bb04ebbca1529
yulin <719655143@qq.com>
2015-06-02 14:46:00 +0800

    提交项目

---
+++ app/controllers/AboutController.php
@@ -0,0 +1,15 @@
+<?php
+
+class AboutController extends ControllerBase
+{
+    public function initialize()
+    {
+        $this->tag->setTitle('About us');
+        parent::initialize();
+    }
+
+    public function indexAction()
+    {
+        echo 'aa';
+    }
+}
